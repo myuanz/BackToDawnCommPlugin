31c9c8daf7c3372b9cac7d95d6e3068efa103ede
myuan <provefars@gmail.com>
2025-08-16 18:29:17 +0800

    init

---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace BackToDawnCommPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Back To The Dawn.exe")]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+}
